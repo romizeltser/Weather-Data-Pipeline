e9efe8e697596874ab4fb9dbf8d7f2f9a6d5b166
romizeltser <166422905+romizeltser@users.noreply.github.com>
2026-03-19 22:04:37 +0200

    Phase 1 Data collection

---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+import requests
+from dotenv import load_dotenv
+import schedule
+from datetime import datetime, timedelta
+import time
+load_dotenv()
+api_key=os.getenv("weather")
+
+if api_key:
+    print("api key loaded successfully")
+else:
+    print("could not load api key")
+
+
+
+def UrlCity(lat, lon):
+    return f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric"
+
+def Cities():
+   
+   cities = [
+       {"name":"TelAviv" , "lat":32.09 , "lon":34.77 },
+       {"name":"RishonLezyon" , "lat":31.9730 , "lon":34.7925 },
+       {"name":"Eilat" , "lat":29.5577 , "lon":34.9519 },
+       {"name":"Yavne" , "lat":31.8776 , "lon":34.7400 },
+       {"name":"Metulla" , "lat":33.2843 , "lon":35.5801 }
+   ]
+    
+   for c in cities:
+       url=UrlCity(c["lat"], c["lon"])
+
+       try:
+           response = requests.get(url, timeout=10)
+           if response.status_code==200: 
+               data = response.json()
+               timestamp=datetime.now()
+               city=data["name"]
+               temperature=data["main"]["temp"]
+               humidity=data["main"]["humidity"]
+               weather=data["weather"][0]["main"]
+
+               print(timestamp)
+               print(city)
+               print(temperature)
+               print(humidity)
+               print(weather)
+
+           elif response.status_code==429: #too many requests
+               continue
+
+       except requests.exceptions.RequestException as e:
+           print(f"error while fetching {c['name']}: {e}")
+
+               
+
+Cities()
+schedule.every(7).minutes.do(Cities)
+start_time=datetime.now()
+finish_time=start_time+timedelta(hours=1)
+while datetime.now()<=finish_time:
+    schedule.run_pending()
+    time.sleep(1)
