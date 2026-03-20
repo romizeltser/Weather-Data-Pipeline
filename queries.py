eeca8105be60c17c89813cfb2c5a05ccdd9ab27c
romizeltser <166422905+romizeltser@users.noreply.github.com>
2026-03-20 16:59:15 +0200

    Added another query function- most humid time

--- queries.py
+++ queries.py
@@ -14,7 +14,6 @@ def avg_temp(city_name):
             SELECT AVG(temperature) FROM weather WHERE city=?
         """
         cursor.execute(sql, (city_name,))
-
         avg_result=cursor.fetchone() #returns tuple
         if avg_result and avg_result[0] is not None:
             return avg_result[0]
@@ -50,4 +49,23 @@ def lowest_temperature():
             return lowest_result
         else:
             logger.warning("could not find data")
+            return None
+
+def most_humid_time():
+    with connect_db() as connection:
+        cursor=connection.cursor()
+
+        sql="""
+            SELECT timestamp, AVG(humidity) as avg_humidity FROM weather GROUP BY timestamp ORDER BY avg_humidity desc
+        """
+        cursor.execute(sql)
+        most_humid_time_result=cursor.fetchone()
+
+        if most_humid_time_result and most_humid_time_result[0] is not None:
+            return {
+                "time": most_humid_time_result[0],
+                "average_humidity": most_humid_time_result[1]
+            }
+        else:
+            logger.warning("Could not find data")
             return None
