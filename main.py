7baf3098426388157ebfc4bd5d21dee20a9a24c4
romizeltser <166422905+romizeltser@users.noreply.github.com>
2026-03-20 03:36:01 +0200

    Fixed 4 spaces

--- main.py
+++ main.py
@@ -19,45 +19,45 @@ def UrlCity(lat, lon):
 
 def Cities():
    
-   cities = [
-       {"name":"TelAviv" , "lat":32.09 , "lon":34.77 },
-       {"name":"RishonLezyon" , "lat":31.9730 , "lon":34.7925 },
-       {"name":"Eilat" , "lat":29.5577 , "lon":34.9519 },
-       {"name":"Yavne" , "lat":31.8776 , "lon":34.7400 },
-       {"name":"Metulla" , "lat":33.2843 , "lon":35.5801 }
-   ]
+    cities = [
+        {"name":"TelAviv" , "lat":32.09 , "lon":34.77 },
+        {"name":"RishonLezyon" , "lat":31.9730 , "lon":34.7925 },
+        {"name":"Eilat" , "lat":29.5577 , "lon":34.9519 },
+        {"name":"Yavne" , "lat":31.8776 , "lon":34.7400 },
+        {"name":"Metulla" , "lat":33.2843 , "lon":35.5801 }
+    ]
 
-   timestamp=datetime.now()
-   for c in cities:
-       url=UrlCity(c["lat"], c["lon"])
+    timestamp=datetime.now()
+    for c in cities:
+        url=UrlCity(c["lat"], c["lon"])
 
-       try:
-           response = requests.get(url, timeout=10)
-           if response.status_code==200: 
-               data = response.json()
-               city=data["name"]
-               temperature=data["main"]["temp"]
-               humidity=data["main"]["humidity"]
-               weather_condition=data["weather"][0]["main"]
-               visibility= data["visibility"]
-               temp_min=data["main"]["temp_min"]
-               temp_max=data["main"]["temp_max"]
+        try:
+            response = requests.get(url, timeout=10)
+            if response.status_code==200: 
+                data = response.json()
+                city=data["name"]
+                temperature=data["main"]["temp"]
+                humidity=data["main"]["humidity"]
+                weather_condition=data["weather"][0]["main"]
+                visibility= data["visibility"]
+                temp_min=data["main"]["temp_min"]
+                temp_max=data["main"]["temp_max"]
                ##
-               print(timestamp)
-               print(city)
-               print(temperature)
-               print(humidity)
-               print(weather_condition)
-               print(visibility)
-               print(temp_min)
-               print(temp_max)
+                print(timestamp)
+                print(city)
+                print(temperature)
+                print(humidity)
+                print(weather_condition)
+                print(visibility)
+                print(temp_min)
+                print(temp_max)
                ##
 
-           elif response.status_code==429: #too many requests
-               continue
+            elif response.status_code==429: #too many requests
+                continue
 
-       except requests.exceptions.RequestException as e:
-           print(f"error while fetching {c['name']}: {e}")
+        except requests.exceptions.RequestException as e:
+            print(f"error while fetching {c['name']}: {e}")
 
                
 
